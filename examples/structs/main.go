bbf9c8fb7d374e3868b4e1c36db75acd09744c06
steevehook <steevehook@gmail.com>
2019-09-23 09:47:51 +0300

    back up

--- examples/structs/main.go
+++ examples/structs/main.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+type person struct {
+	Name string
+	Age  uint
+}
+
+func main() {
+	people := []struct{
+		name string
+		age uint
+	} {
+		{name: "John", age: 21},
+		{"Jane", 19}, // don't use this
+	}
+	fmt.Printf("%+v\n", person{Name: "Steve", Age: 24})
+	fmt.Printf("%+v", people)
+}
